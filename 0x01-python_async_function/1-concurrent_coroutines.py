94798f702ac5430feb609eeab5f0c4994b3af953
xavier <xavierkanu@yahoo.com>
2023-02-06 14:06:56 +0100

    new lines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" The basics of async """
+
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn wait_random n times with the specified max_delay.
+    """
+    queue, array = [], []
+
+    for _ in range(n):
+        queue.append(wait_random(max_delay))
+
+    for q in asyncio.as_completed(queue):
+        result = await q
+        array.append(result)
+
+    return array
